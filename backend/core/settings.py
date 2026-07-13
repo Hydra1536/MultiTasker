286a3143e27719dcaeb889f9de7f2775188e5217
MD Rezaul Karim <152043654+Hydra1536@users.noreply.github.com>
2026-07-14 00:42:37 +0600

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -84,6 +84,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://multitasker1.onrender.com", 
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
